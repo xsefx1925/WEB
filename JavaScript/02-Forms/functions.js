a762bde548807f66849a4a1a87246e5cec6bc0b5
xsefx1925 <xsefx1925@gmail.ru>
2025-09-13 12:37:32 +0300

    Clock.DONE

--- JavaScript/02-Forms/functions.js
+++ JavaScript/02-Forms/functions.js
@@ -55,3 +55,35 @@ reader.onload = function (e)
 }
 reader.readAsDataURL(filename.files[0]);
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////                         Timer                           ////////////////////////
+
+document.body.onload = function tick_timer()
+{
+    let time = new Date();
+    document.getElementById("full-time").innerHTML = time;
+
+    //            current-time
+    document.getElementById("hours").innerHTML = addLeadingZero(time.getHours());
+    document.getElementById("minutes").innerHTML = addLeadingZero(time.getMinutes());
+    document.getElementById("seconds").innerHTML = addLeadingZero(time.getSeconds());
+
+    //             current-day
+    document.getElementById("year").innerHTML = addLeadingZero(time.getFullYear());
+    document.getElementById("month").innerHTML = addLeadingZero(time.getMonth()+1);
+    document.getElementById("day").innerHTML = addLeadingZero(time.getDate());
+
+    //               week-day
+    document.getElementById("weekday").innerHTML = time.toLocaleDateString("ru", { weekday: 'long' });
+
+    //               Checkboxes:
+    document.getElementById("current-date").style.visibility = document.getElementById("show-date").checked ? "visible" : "hidden";
+    document.getElementById("weekday").style.visibility = document.getElementById("show-weekday").checked ? "visible" : "hidden";
+
+
+    setTimeout(tick_timer, 100);
+}
+function addLeadingZero(number)
+{
+    return number < 10 ? "0"+number : number;
+}
