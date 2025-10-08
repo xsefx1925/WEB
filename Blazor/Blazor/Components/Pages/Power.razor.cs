b98592e9f2fd393e461b2e2d77ea1783eba09974
xsefx1925 <xsefx1925@gmail.ru>
2025-10-08 10:08:09 +0300

    Power components splitted to files

---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		double number;
+		double exponent;
+		double power;
+		void Calculate()
+		{
+			power = Math.Pow(number, exponent);
+		}
+	}
+}
